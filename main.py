c92799449ebe6c0fa5195bb29fb7c809ab7940a9
cmartinjr2008 <cmartinjr2008@gmail.com>
2024-04-11 08:16:29 -0700

    Finished bookbot project

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("Hello World")
+def countLetters(splitBook):
+    letterCount = {}
+
+    #for x in range(len(splitBook)):
+    for letter in splitBook:
+        if letter in letterCount.keys():
+            letterCount[letter] += 1
+                    
+        else:
+            letterCount[letter] = 1
+                              
+    for letter in letterCount:
+        print("The", letter, "character was found ", letterCount[letter], "times")
+    
+
+with open('books/frankenstein.txt') as f:
+    bookFile = f.read()
+    wordSplit = []
+    lettersplit = []
+    numberOfWords = 0
+    wordSplit = bookFile.split()
+    
+    for words in wordSplit:
+        if words.isalpha():
+            for letter in words:
+                lettersplit += letter.lower()
+        
+        numberOfWords += 1
+    
+    print("--Analyzing Uploaded Book--")
+    print("There are a total of:", numberOfWords, "in this book")
+    print("Here is the breakdown of letters: ")
+    countLetters(lettersplit)
+        
+    
+    
+    
+    
+
+
+
+
